5e32b74bbb04319524884be30d3272b1d7b93b78
Samantha Carow <samantha@preact.com>
2016-01-06 20:49:43 -0800

    basic router

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ To install packages: `npm install`
 
 To view in browser: `http://localhost:8080/`
 
+To add `react-boilerplate` as upstream (in order to pull changes from `react-boilerplate`): `git remote add upstream git@github.com:scarow/react-boilerplate.git`
--- index.jsx
+++ index.jsx
@@ -1,5 +1,13 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
-import Hello from './Hello.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router';
+import routes from './routes.jsx';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+import Root from './Root.jsx';
+let history = createBrowserHistory();
 
-ReactDOM.render(<Hello />, document.getElementById('content'))
+ReactDOM.render(
+  <Router history={history}>{routes}</Router>,
+  document.getElementById('content')
+);
+
